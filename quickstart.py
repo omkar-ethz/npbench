de5885f92fd50e3afe08cbbe412a46776e98c951
Omkar Z <omzade@student.ethz.com>
2022-12-14 18:11:09 +0100

    Modify quickstart.py to support num_threads as well

--- quickstart.py
+++ quickstart.py
@@ -1,17 +1,16 @@
 import argparse
+import os
 
 from multiprocessing import Process
 from npbench.infrastructure import (Benchmark, generate_framework, LineCount,
                                     Test, utilities as util)
 
-def run_benchmark(benchname, fname, preset, validate, repeat, timeout):
-        frmwrk = generate_framework(fname)
-        numpy = generate_framework("numpy")
-        bench = Benchmark(benchname)
-        lcount = LineCount(bench, frmwrk, numpy)
-        lcount.count()
-        test = Test(bench, frmwrk, numpy)
-        test.run(preset, validate, repeat, timeout)
+def run_benchmark(benchname, fname, preset, validate, repeat, timeout, num_threads):
+    frmwrk = generate_framework(fname)
+    numpy = generate_framework("numpy")
+    bench = Benchmark(benchname)
+    test = Test(bench, frmwrk, numpy)
+    test.run(preset, validate, repeat, timeout, num_threads=num_threads)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -27,27 +26,42 @@ if __name__ == "__main__":
                         nargs="?",
                         default=True)
     parser.add_argument("-r", "--repeat", type=int, nargs="?", default=10)
-    parser.add_argument("-t", "--timeout", type=float, nargs="?", default=10.0)
+    parser.add_argument("-t", "--timeout", type=float, nargs="?", default=200.0)
     parser.add_argument("-d", "--dace", type=util.str2bool, nargs="?", default=True)
+    parser.add_argument("-n", "--num-threads", type=int, nargs="?", default=1)
     args = vars(parser.parse_args())
 
+    print("num_threads=", args["num_threads"])
+    os.environ['OMP_NUM_THREADS'] = str(args["num_threads"])
+    os.environ['MKL_NUM_THREADS'] = str(args["num_threads"])
+    os.environ["OPENBLAS_NUM_THREADS"] = str(args["num_threads"])
+    os.environ["VECLIB_MAXIMUM_THREADS"] = str(args["num_threads"])
+    os.environ["NUMEXPR_NUM_THREADS"] = str(args["num_threads"])
+
     benchmarks = [
-        'adi', 'arc_distance', 'atax', 'azimint_naive', 'bicg', 'cavity_flow',
+        'adi', 'arc_distance', 'atax', 'bicg', 'cavity_flow',
         'cholesky2', 'compute', 'doitgen', 'floyd_warshall', 'gemm', 'gemver',
         'gesummv', 'go_fast', 'hdiff', 'jacobi_2d', 'lenet', 'syr2k', 'trmm',
         'vadv'
     ]
 
-    frameworks = ["numpy", "numba"]
-    if args['dace']:
-        frameworks.append("dace_cpu")
+
+    frameworks = ["numpy", "dace_cpu", "dace_cpu_tasking", "dace_cpu_dynamic_schedule"]
 
     for benchname in benchmarks:
         for fname in frameworks:
+            # set environment variable openmp_tasking
+            if fname == "dace_cpu_tasking":
+                os.environ['DACE_compiler_cpu_openmp_tasking'] = "true"
+            elif fname == "dace_cpu":
+                os.environ['DACE_compiler_cpu_openmp_tasking'] = "false"
+            elif fname == "dace_cpu_dynamic_schedule":
+                os.environ['DACE_compiler_cpu_openmp_tasking'] = "false"
+                os.environ['DACE_compiler_cpu_openmp_dynamic_schedule'] = "true"
             p = Process(
                 target=run_benchmark,
                 args=(benchname, fname, args["preset"],
-                    args["validate"], args["repeat"], args["timeout"])
+                    args["validate"], args["repeat"], args["timeout"], args["num_threads"])
             )
             p.start()
             p.join()
